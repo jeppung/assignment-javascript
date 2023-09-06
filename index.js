059c0084a052b23243db80d726c9b6dfdd7d2704
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-09-06 10:29:49 +0700

    feat: adding initial menu

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const readline = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+
+const startOnlineBank = async () => {
+    while(true) {
+        console.log("Welcome to DIGI ATM")
+        console.log("Menu:")
+        console.log("1. Register")
+        console.log("2. Login")
+        let userRef = await readline.question("Input: ")
+    }
+}
+
+startOnlineBank()
