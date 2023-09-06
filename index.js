483fe61fca7e12e9d82a8a94d7c71b3cc9e2827d
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-09-06 12:20:55 +0700

    feat: modifying index.js, moving menu to class

--- index.js
+++ index.js
@@ -1,72 +1,12 @@
-import { createInterface } from 'readline/promises';
-import Authentication from './models/authentication.js';
-
-const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+import Menu from './models/menu.js';
+import User from './models/user.js';
 
 
 const startOnlineBank = async () => {
+    let user = await Menu.authMenu()
 
-    while(true) {
-        console.log("Welcome to DIGI ATM")
-        console.log("Menu:")
-        console.log("1. Register")
-        console.log("2. Login")
-        let input = await readline.question("Input: ")
-
-        switch(parseInt(input)) {
-            case 1 : {
-                while(true){
-                    console.clear()
-                    console.log("Register")
-                    let username = await readline.question("username: ")
-                    if (input != "") {
-                        console.clear()
-                        console.log("Register")
-                        let pin = await readline.question("pin: ")
-                        
-                        let isValid = Authentication.validation(username, pin)
-                        if (isValid) {
-                            try{
-                                let res = await Authentication.register(username, pin)
-                                if(!res.ok) throw new Error(res.statusText)
-
-                                console.clear()
-                                console.log("Register Success")
-                                break
-                            }catch(err){
-                                console.log(err)
-                            }
-                        }
-                    }
-                }
-                break
-            }
-            case 2 : {
-                while(true){
-                    console.clear()
-                    console.log("Login")
-                    let username = await readline.question("username: ")
-                    console.clear()
-                    console.log("Login")
-                    let pin = await readline.question("pin: ")
-
-                    try{
-                        let res = await Authentication.login(username, pin)
-                        if (!res.ok) throw new Error(res.statusText)
-
-                        console.clear()
-                        console.log("Login Success")
-                        break
-                    }catch(err) {
-                        console.log(err)
-                    }
-                }
-                break
-            }
-        }
+    if(user instanceof User && user.isAuth == true) {
+        Menu.mainMenu(user)
     }
 }
 
