df02d9729a1c5ef5201b0cf9951c7639ff84318c
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-09-06 13:11:29 +0700

    feat: add getBalance method

--- models/user.js
+++ models/user.js
@@ -1,8 +1,16 @@
 class User {
-    constructor(_username, _pin, _isAuth) {
+    constructor(_id, _username, _pin, _isAuth) {
+        this.id = _id
         this.username = _username,
         this.pin = _pin,
         this.isAuth = _isAuth
+    }
+
+
+    async getBalance() {
+        return fetch(`http://localhost:1337/api/balance/${this.id}`, {
+            method: "GET"
+        })
     }
 }
 
